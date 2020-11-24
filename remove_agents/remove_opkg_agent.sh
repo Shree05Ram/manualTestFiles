5d7aebe70b55453957eb12703d75889935bf6bbe
Shree05Ram <shreeram@aikaan.io>
2020-11-24 16:50:35 +0530

    opkg agent removal updated

--- remove_agents/remove_opkg_agent.sh
+++ remove_agents/remove_opkg_agent.sh
@@ -1,7 +1,16 @@
-user="$(id -un 2>/dev/null || true)"
+#!/bin/sh
 
-INSTALL_DIR=$1
-CONFIG_DIR=$2
+DEFAULT_DIR=/opt/aikaan
+BIN_FILE=$DEFAULT_DIR/bin/opkg
+CUSTOM_DIR=$1/aikaan
+CONFIGURE_DIR=$2/aikaan
+
+#set -v
+
+user="$(id -un 2>/dev/null || true)"
+#set -v
+
+user="$(id -un 2>/dev/null || true)"
 
 if [ "$user" != 'root' ]; then
         echo "Should be root user to run the script"
@@ -13,9 +22,19 @@ else
         systemctl disable aikaan-agent-opkg-updater.timer
         systemctl stop aikaan.service
         systemctl disable aikaan.service
-        $INSTALL_DIR/opt/aikaan/bin/opkg -f $INSTALL_DIR/opt/aikaan/etc/opkg.conf remove aiagent
-        $INSTALL_DIR/opt/aikaan/bin/opkg -f $INSTALL_DIR/opt/aikaan/etc/opkg.conf clean
-        rm -rf /etc/systemd/system/aikaan*
-        rm -rf $INSTALL_DIR/opt/aikaan
+        if [ ! -z $CUSTOM_DIR ] && [ ! -f "$BIN_FILE" ]; then
+                $CUSTOM_DIR/$BIN_FILE -f $CUSTOM_DIR/aikaan/etc/opkg.conf remove aiagent
+                $CUSTOM_DIR/$BIN_FILE -f $CUSTOM_DIR/aikaan/etc/opkg.conf clean
+        else
+                $BIN_FILE -f $DEFAULT_DIR/etc/opkg.conf remove aiagent
+                $BIN_FILE -f $DEFAULT_DIR/etc/opkg.conf clean
+                rm -rf $DEFAULT_DIR
+        fi
+
+        #if [[ ! -f "$CUSTOM_DIR" ] || ["$CONFIG_DIR" ]]; then
+        #              rm -rf $CUSTOM_DIR $CONFIG_DIR
+        #fi
+        
+        rm -rf /etc/systemd/system/aikaan* $CUSTOM_DIR $CONFIG_DIR
         echo "aiagent is uninstalled and the directories are clean"
 fi
