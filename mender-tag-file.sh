962b0991a0a5ae72f2c3c54dd29223124a23cb8f
Shree05Ram <45550743+Shree05Ram@users.noreply.github.com>
2021-08-20 13:21:16 +0530

    Update mender-tag-file.sh
    Provide location to place the file

--- mender-tag-file.sh
+++ mender-tag-file.sh
@@ -1,4 +1,5 @@
 # name of the file should be mender-invetory-test.sh
+# The file should be placed in /opt/aikaan/bin/support/inventory dir
 #!/bin/sh
 echo "abc=ghi"
 echo "device_name=yum" #This would change the name of the device to "yum"
